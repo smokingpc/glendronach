d50adb24f055b4ec1082918b386d3c095089e83f
SmokingPC <smokingpc@gmail.com>
2023-03-02 16:08:20 +0800

    add missed files in previous commit

---
+++ src/SpcNvme/AdapterPnpHandler.cpp
@@ -0,0 +1,32 @@
+#include "pch.h"
+
+void AdapterPnp_QueryCapHandler(PSPCNVME_SRBEXT srbext)
+{
+    PSTOR_DEVICE_CAPABILITIES_EX cap = (PSTOR_DEVICE_CAPABILITIES_EX)srbext->DataBuf;
+    cap->Version = STOR_DEVICE_CAPABILITIES_EX_VERSION_1;
+    cap->Size = sizeof(STOR_DEVICE_CAPABILITIES_EX);
+    cap->DeviceD1 = 0;
+    cap->DeviceD2 = 0;
+    cap->LockSupported = 0;
+    cap->EjectSupported = 0;
+    cap->Removable = 1;         //support RemoveAdapter
+    cap->DockDevice = 0;
+    cap->UniqueID = 0;
+    cap->SilentInstall = 1;     //support silent install on DeviceManager UI
+    cap->SurpriseRemovalOK = 1; //support Adapter SurpriseRemove (hot plug)
+    cap->NoDisplayInUI = 0;     //should this adapter be shown on DeviceManager UI?
+    cap->Address = 0;
+    cap->UINumber = 0xFFFFFFFF;
+    srbext->Srb->SrbStatus = SRB_STATUS_SUCCESS;
+}
+
+void AdapterPnp_RemoveHandler(PSPCNVME_SRBEXT srbext)
+{
+    srbext->DevExt->Teardown(true);
+    srbext->Srb->SrbStatus = SRB_STATUS_SUCCESS;
+}
+void AdapterPnp_SurpriseRemoveHandler(PSPCNVME_SRBEXT srbext)
+{
+    srbext->DevExt->Teardown();
+    srbext->Srb->SrbStatus = SRB_STATUS_SUCCESS;
+}
---
+++ src/SpcNvme/AdapterPnpHandler.h
@@ -0,0 +1,4 @@
+#pragma once
+void AdapterPnp_QueryCapHandler(PSPCNVME_SRBEXT srbext);
+void AdapterPnp_RemoveHandler(PSPCNVME_SRBEXT srbext);
+void AdapterPnp_SurpriseRemoveHandler(PSPCNVME_SRBEXT srbext);
