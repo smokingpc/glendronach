2bb1305d167c959c2b1ccaf41b7dea51e27bb529
SmokingPC <smokingpc@gmail.com>
2024-02-04 13:12:38 +0800

    fix new operator allocation

--- src/WdmUtils/CppSupport.cpp
+++ src/WdmUtils/CppSupport.cpp
@@ -8,29 +8,42 @@
 //The operator declaration is already built in VC++, but WDM doesn't provide implementation.
 //In usermode application, this implementation is provided in usermode CRT lib.
 
+static FORCEINLINE void* AllocateKernelMemory(
+    size_t size,
+    POOL_TYPE type,
+    ULONG tag)
+{
+//Static DriverVerifier will fail if use ExAllocatePoolWithTag.
+//replace it by ExAllocatePoolUninitialized can pass it. 
+    void* buffer = ExAllocatePoolUninitialized(type, size, tag);
+    if (NULL != buffer)
+        RtlZeroMemory(buffer, size);
+    return buffer;
+}
+
 //usage: MyClass *ptr = new MyClass();
 void* __cdecl operator new (size_t size)
 {
-    return ExAllocatePoolWithTag(PagedPool, size, CPP_TAG);
+    return AllocateKernelMemory(size, PagedPool, CPP_TAG);
 }
 
 
 //usage: MyClass *ptr = new(NonPagedPool, MY_POOL_TAG) MyClass();
 void* operator new (size_t size, POOL_TYPE type, ULONG tag)
 {
-    return ExAllocatePoolWithTag(type, size, tag);
+    return AllocateKernelMemory(size, type, tag);
 }
 
 //usage: char *ptr = new char[10];
 void* __cdecl operator new[](size_t size)
 {
-    return ExAllocatePoolWithTag(PagedPool, size, CPP_TAG);
+    return AllocateKernelMemory(size, PagedPool, CPP_TAG);
 }
 
 //usage: char *ptr = new(NonPagedPool, MY_POOL_TAG) char[10];
 void* operator new[](size_t size, POOL_TYPE type, ULONG tag)
 {
-    return ExAllocatePoolWithTag(type, size, tag);
+    return AllocateKernelMemory(size, type, tag);
 }
 
 //usage: delete ptr;
